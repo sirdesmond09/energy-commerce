061cdaa0d1067f69f97f8315fb32ff95416b8a5e
sirdesmond09 <sirdesmond09@gmail.com>
2023-02-07 09:01:40 +0100

    add in lines

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([ProductCategory, ProductGallery, Location])
+admin.site.register([ProductCategory, ProductGallery, Location, ProductComponent])
 
 
+
+class ComponentsAdmin(admin.TabularInline):
+    model=ProductComponent
+    
+class GalleryAdmin(admin.TabularInline):
+    model = ProductGallery
+    
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["name", "is_deleted"]
     list_editable = ["is_deleted"]
+    inlines = [ProductComponent,ProductGallery]
     
