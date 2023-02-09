a1d6f40c078b877cd514b654bcba953ef45bfdd1
sirdesmond09 <sirdesmond09@gmail.com>
2023-02-09 10:27:07 +0100

    update paginator

--- config/settings.py
+++ config/settings.py
@@ -164,8 +164,8 @@ class Common(Configuration):
         ),
 
 
-        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-        "PAGE_SIZE": 100,
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+        
     }
 
     SIMPLE_JWT = {
