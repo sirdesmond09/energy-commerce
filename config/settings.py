c0f9b40707f2e6a79166a88032c82f78753077c7
sirdesmond09 <sirdesmond09@gmail.com>
2023-02-09 22:07:48 +0100

    add email

--- config/settings.py
+++ config/settings.py
@@ -213,6 +213,14 @@ class Common(Configuration):
     MEDIA_URL = '/media/'
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = "smtp.mailgun.org"
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = 465
+    EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = False 
+    
 
 
 
