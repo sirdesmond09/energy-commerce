49f46b085e5df72a85dffb7e43feab448bdbb32e
sirdesmond09 <sirdesmond09@gmail.com>
2023-04-12 12:53:28 +0100

    upate

--- accounts/models.py
+++ accounts/models.py
@@ -37,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name     = models.CharField(_('last name'),max_length = 250)
     role          = models.CharField(_('role'), max_length = 255, choices=ROLE_CHOICES)
     email         = models.EmailField(_('email'), unique=True)
-    phone         = models.CharField(_('phone'), max_length = 100, unique = True, validators=[phone_regex])
+    phone         = models.CharField(_('phone'), max_length = 100, null = True, validators=[phone_regex])
     favourite    = models.ManyToManyField("main.Product", blank=True)
     vendor_status = models.CharField(max_length=250, 
                                      blank=True, 
