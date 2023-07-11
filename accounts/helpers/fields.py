d8d6a0f41c94e11283ba4ec2d3532b29b629f450
sirdesmond09 <sirdesmond09@gmail.com>
2023-07-11 09:40:02 +0100

    debug

--- accounts/helpers/fields.py
+++ accounts/helpers/fields.py
@@ -8,6 +8,8 @@ class PDFBase64File(Base64FileField):
     ALLOWED_TYPES = ['pdf']
 
     def get_file_extension(self, filename, decoded_file):
+        print(filename)
+        
         try:
             PyPDF2.PdfFileReader(io.BytesIO(decoded_file))
         except Exception as e:
