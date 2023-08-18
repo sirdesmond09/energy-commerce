579709095e6a99d85f219b7898679a20d0b30bb2
sirdesmond09 <sirdesmond09@gmail.com>
2023-08-18 10:51:42 +0100

    fix static files

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,7 @@ class Common(Configuration):
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    WHITENOISE_MANIFEST_STRICT = False
     
     
 
