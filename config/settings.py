94735fdab7e7de326863fcc883af8d79824c94d6
sirdesmond09 <sirdesmond09@gmail.com>
2023-08-21 12:10:30 +0100

    update vendor stat

--- config/settings.py
+++ config/settings.py
@@ -140,8 +140,8 @@ class Common(Configuration):
     # https://docs.djangoproject.com/en/4.1/howto/static-files/
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    WHITENOISE_MANIFEST_STRICT = False
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+    # WHITENOISE_MANIFEST_STRICT = False
     
     
 
